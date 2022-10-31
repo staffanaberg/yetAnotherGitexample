79ecc8630fc5b24ac6519412e111eb085ab3cbba
staff <staffan.aaberg@gmail.com>
2022-10-31 11:44:17 +0100

    Added Calculator and method add

---
+++ src/helloWorld/Calculator.java
@@ -0,0 +1,18 @@
+package helloWorld;
+
+public class Calculator {
+
+    private int first;
+    private int second;
+
+    public Calculator(int firstNumber, int secondNumber) {
+        first = firstNumber;
+        second = secondNumber;
+    }
+
+    public int add() {
+        return first + second;
+    }
+
+
+}
--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -9,6 +9,8 @@ public class HelloWorld {
 
         //Yet another comment
 
+        //Something else
+
     }
 
 }
